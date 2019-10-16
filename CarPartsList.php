9e51cf3ed75f9325413d6e3237dfada4a523a482
TECDOC MYSQL <info@tecdoc.ru>
2019-10-16 20:08:24 +0300

    Update and rename getPhotoByPartNumber.php to CarPartsList.php

---
+++ CarPartsList.php
@@ -0,0 +1,36 @@
+<?
+/*
+Функция CarPartsList
+Список запчастей на указанную модификацию автомобиля. KID: 2 - легковые, 16 - коммерческие и грузовые, 777 - мото, 14 - двигатель, 19 - ось. По умолчанию "Пробел". Можно ставить точку с запятой (;), запятую (,), вертикальную линию (|).
+
+Адрес API: https://partsapi.ru/api.php?act=CarPartsList&typeid={typeid}&kid={kid}&del={del}&key={key}
+
+ПАРАМЕТР	ТИП	ДЛИНА	ОПИСАНИЕ
+typeid	string	3	Тип транспортного средства
+kid	string	7	Идентификатор автомобиля в Текдок
+del	string	1	Разделитель бренда и номера. По умолчанию "Пробел"
+key	string	16	Ключ для авторизации
+*/
+
+class PARTSAPI
+{
+    public static function CarPartsList($typeid, $kid, $del, $key) {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://partsapi.ru/api.php?act=CarPartsList&typeid=$typeid&kid=$kid&del=$del&key=$key",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => false,
+        ));
+        $response = curl_exec($curl);
+        $response = json_decode($response, false);
+        curl_close($curl);
+        return $response;
+    }
+}
+$typeid="100383";
+$kid="2";
+$del="" "";
+$key="test";
+$result=PARTSAPI::CarPartsList($typeid, $kid, $del, $key);
+print_r($result);
+?>
